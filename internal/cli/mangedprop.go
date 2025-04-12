e39bfcf104c80a3759140c778d7fa4395f112d0d
Ethan P. <eth-p+git@hidden.email>
2025-04-12 15:06:28 -0700

    feat: Add completions for cluster/context/user

--- internal/cli/mangedprop.go
+++ internal/cli/mangedprop.go
@@ -107,7 +107,8 @@ func createManagedPropertySwitchCommand(cmd *cobra.Command, prop *managedPropert
 	}
 
 	cmd.Args = cobra.RangeArgs(0, 1)
-	cmd.ValidArgsFunction = nil
+	cmd.ValidArgsFunction = createManagedPropertyCompletionFunc(prop)
+
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
 		ksel, err := Kubesel()
 		if err != nil {
@@ -164,6 +165,85 @@ func createCommandNameAndAliases(name string, prop *managedProperty[any]) (strin
 	}
 
 	return name, aliases
+}
+
+func createManagedPropertyCompletionFunc(prop *managedProperty[any]) cobra.CompletionFunc {
+	if prop.GetItemNames == nil {
+		return nil
+	}
+
+	return func(cmd *cobra.Command, args []string, toComplete string) ([]cobra.Completion, cobra.ShellCompDirective) {
+		items, err := getCompletionItemsFromNames(prop)
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveError
+		}
+
+		// Sort the items.
+		slices.SortFunc(items, func(a, b completionItem) int {
+			return strings.Compare(a.name, b.name)
+		})
+
+		// Filter out the returned options by fuzzy matching.
+		if toComplete != "" {
+			items = filterCompletionItemsByFuzzy(items, toComplete)
+		}
+
+		// Return the matching names.
+		cobraComps := make([]cobra.Completion, len(items))
+		for i, item := range items {
+			cobraComps[i] = item.asCobraCompletion()
+		}
+
+		return cobraComps, cobra.ShellCompDirectiveNoFileComp
+	}
+}
+
+func filterCompletionItemsByFuzzy(items []completionItem, query string) []completionItem {
+	matches := fuzzy.MatchesFunc(items, query, func(ci completionItem) string {
+		return ci.name
+	})
+
+	// Sort the matches by score.
+	slices.SortFunc(matches, func(a, b fuzzy.MatchResult[completionItem]) int {
+		if a.Score != b.Score {
+			return b.Score - a.Score
+		}
+
+		return strings.Compare(a.Item.name, b.Item.name)
+	})
+
+	// Return the original completion items.
+	result := make([]completionItem, len(matches))
+	for i, match := range matches {
+		result[i] = match.Item
+	}
+
+	return result
+}
+
+type completionItem struct {
+	name string
+	// TODO: descriptions
+}
+
+func (c completionItem) asCobraCompletion() string {
+	return c.name
+}
+
+// getCompletionItemsFromNames returns completion items by using the
+// [managedProperty.GetItemNames] function to fetch the list of valid names.
+func getCompletionItemsFromNames(prop *managedProperty[any]) ([]completionItem, error) {
+	names, err := prop.GetItemNames()
+	if err != nil {
+		return nil, err
+	}
+
+	completions := make([]completionItem, len(names))
+	for i, name := range names {
+		completions[i].name = name
+	}
+
+	return completions, nil
 }
 
 // itemInfoGenerator is a function that creates an iterator over some type.
--- internal/fuzzy/match.go
+++ internal/fuzzy/match.go
@@ -10,16 +10,21 @@ import (
 
 var ErrNoMatch = errors.New("no match")
 
-// Matches runs a fuzzy find query against a list of strings, returning
-// the strings which can be matched by the query.
-func Matches(items []string, query string) []string {
-	var result []string
+type MatchResult[T any] struct {
+	Item  T
+	Score int
+}
+
+// MatchesFunc runs a fuzzy find query against a list of items, returning
+// the [MatchResults] for all items matched by the query.
+func MatchesFunc[T any](items []T, query string, getName func(T) string) []MatchResult[T] {
+	var result []MatchResult[T]
 
 	// Run every item through fzf's match algorithm with the query.
 	queryRunes := []rune(query)
 	slab := util.MakeSlab(1024, 8192)
 	for _, item := range items {
-		chars := util.ToChars([]byte(item))
+		chars := util.ToChars([]byte(getName(item)))
 		match, _ := algo.FuzzyMatchV2(
 			false,      // not case sensitive
 			true,       // normalized
@@ -34,10 +39,35 @@ func Matches(items []string, query string) []string {
 			continue
 		}
 
-		result = append(result, item)
+		result = append(result, MatchResult[T]{
+			Item:  item,
+			Score: match.Score,
+		})
 	}
 
 	return result
+}
+
+// Matches runs a fuzzy find query against a list of strings, returning
+// the [MatchResult]s for all strings matched by the query.
+func Matches(items []string, query string) []MatchResult[string] {
+	return MatchesFunc(items, query, func(s string) string { return s })
+}
+
+// Matches runs a fuzzy find query against a list of strings, returning
+// the strings which can be matched by the query.
+func StringMatches(items []string, query string) []string {
+	matches := Matches(items, query)
+	if len(matches) == 0 {
+		return []string{}
+	}
+
+	results := make([]string, len(matches))
+	for i, match := range matches {
+		results[i] = match.Item
+	}
+
+	return results
 }
 
 func MatchOneOrPick(items []string, query string) (string, error) {
@@ -51,7 +81,7 @@ func MatchOneOrPick(items []string, query string) (string, error) {
 		}
 
 		// Fuzzy match?
-		items = Matches(items, query)
+		items = StringMatches(items, query)
 		switch len(items) {
 		case 0:
 			return "", ErrNoMatch
@@ -67,4 +97,52 @@ func MatchOneOrPick(items []string, query string) (string, error) {
 	}
 
 	return Pick(items, &pickOpts)
+}
+
+// SortedMatchesFunc runs a fuzzy find query against a slice, returning the
+// items which can be matched by the query. The returned slice is sorted by
+// score.
+func SortedMatchesFunc[T any](
+	items []T,
+	query string,
+	getName func(*T) string,
+	compare func(a, b MatchResult[T]) int,
+) []T {
+	var matches []MatchResult[T]
+
+	// Run every item through fzf's match algorithm with the query.
+	queryRunes := []rune(query)
+	slab := util.MakeSlab(1024, 8192)
+	for _, item := range items {
+		chars := util.ToChars([]byte(getName(&item)))
+		match, _ := algo.FuzzyMatchV2(
+			false,      // not case sensitive
+			true,       // normalized
+			false,      // ??
+			&chars,     // what to match against
+			queryRunes, // the query
+			false,      // do not return positions
+			slab,       // slab allocator
+		)
+
+		if match.Score < 1 {
+			continue
+		}
+
+		matches = append(matches, MatchResult[T]{
+			Item:  item,
+			Score: match.Score,
+		})
+	}
+
+	// Sort the matches.
+	slices.SortFunc(matches, compare)
+
+	// Return the matched items.
+	result := make([]T, len(matches))
+	for i, match := range matches {
+		result[i] = match.Item
+	}
+
+	return result
 }
